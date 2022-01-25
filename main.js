6bfb783ed8791e13b100c7f394bdb9f9b14f759d
Aaravsharma163 <84375040+Aaravsharma163@users.noreply.github.com>
2022-01-25 14:07:40 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,30 @@
+function setup()
+{
+  video = createCapture(VIDEO);
+  video.size(550,500);
+
+  canvas = createCanvas(550,500);
+  canvas.position(560,150);
+
+  poseNet = ml5.poseNet(video,modelLoaded);
+  poseNet.on('pose',gotPoses);
+}
+
+function draw()
+{
+    background('#1b444a')
+}
+
+function modelLoaded()
+{
+    console.log('poseNet is initialized');
+}
+
+function gotPoses(results)
+{
+    if(results.length > 0)
+
+    {
+        console.log(results);
+    }
+}
